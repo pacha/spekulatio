4cb3defbfc0ea9d77da319600373390c17685972
Andres Sopena Perez <andres.sopena@trivago.com>
2024-09-21 11:27:11 +0200

    chore: set Python versions and make first test run

--- spekulatio/models/node.py
+++ spekulatio/models/node.py
@@ -1,7 +1,9 @@
 
+from typing import Optional
+from dataclasses import field
 from dataclasses import dataclass
 
 @dataclass
 class Node:
-    parent: "Node" | None = None
-    children: list["Node"] = []
+    parent: Optional["Node"] = None
+    children: list["Node"] = field(default_factory=list)
